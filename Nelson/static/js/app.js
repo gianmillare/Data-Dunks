2a1a7e9a54517eedd3f74001a4f63bd1fc26520f
nelsont87 <nelsont87@gmail.com>
2019-12-20 22:04:36 -0800

    test

--- Nelson/static/js/app.js
+++ Nelson/static/js/app.js
@@ -1,23 +1,50 @@
 
 
 function pvp() {
-    var selector = d3.select("#stats1").append("table");
-    var thead = table.append("thead");
-    var tbody = table.append("tbody");
+
     d3.json("/pvpstats" ).then(function(data) {
         console.log(data);
-		Object.entries(data).forEach(([key, value]) => {
-			thead.append("tr")
-                .selectAll("th")
-                .data(key.index)
-                .enter()
-                .append("th")
-                .text(function(d) { console.log(d);return d; });
-		});
+		
+        function tabulate(data, columns) {
+  
+            var table = d3.select('#stats1').append('table')
+            var thead = table.append('thead')
+            var	tbody = table.append('tbody');
+    
+            // append the header row
+            thead.append('tr')
+              .selectAll('th')
+              .data(columns)
+              .enter()
+              .append('th')
+              .text(function (column) { return column; });
+    
+            // create a row for each object in the data
+            var rows = tbody.selectAll('tr')
+              .data(data[0].data)
+              .enter()
+              .append('tr');
+    
+            // create a cell in each row for each column
+            var cells = rows.selectAll('td')
+              .data(function (row,i) {return row; })
+              .enter()
+              .append('td')
+              .text(function (d) { return d; });
+    
+          return table;
+        }
+    
+        // render the table(s)
+         tabulate(data[0], data[0].index); // data, columns table 
+                
+        
+
     });
     
 }
-
+// <th>${d.index[13]}</th><th>${d.index[28]}</th><th>${d.index[32]}</th><th>${d.index[34]}</th><th>${d.index[49]}</th>
+                // BPM(12), DBPM(13), OBPM(28), PER(32), PTS(34), EFG%(49)
 
 
 
