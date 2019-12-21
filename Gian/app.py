95fd728bc5b0dee0b8cc95c266d75efa8165b2ec
Gian Millare <gianmillare@Gians-MacBook-Pro.local>
2019-12-21 05:48:24 -0800

    revising and addadding to app.js and app.py

--- Gian/app.py
+++ Gian/app.py
@@ -59,9 +59,49 @@ def info():
     return jsonify(player_list)
 
 
-@app.route("/tvt.html")
+@app.route("/tvt.html", methods=["GET", "POST"])
 def teamroster():
+    if request.method == "POST":
+        player1 = request.form["player_one"]
+        player2 = request.form["player_two"]
+        player3 = request.form["player_three"]
+        player4 = request.form["player_four"]
+        player5 = request.form["player_five"]
+        player6 = request.form["player_six"]
+        player7 = request.form["player_seven"]
+        player8 = request.form["player_eight"]
+        player9 = request.form["player_nine"]
+        player10 = request.form["player_ten"]
+        session["player_one"] = player1
+        session["player_two"] = player2
+        session["player_three"] = player3
+        session["player_four"] = player4
+        session["player_five"] = player5
+        session["player_six"] = player6
+        session["player_seven"] = player7
+        session["player_eight"] = player8
+        session["player_nine"] = player9
+        session["player_ten"] = player10
     return render_template("tvt.html")
+
+@app.route("/tvtstats")
+def team():
+    team_list = []
+    player1= session.get("player_one")
+    player2 = session.get("player_two")
+    player3 = session.get("player_three")
+    player4 = session.get("player_four")
+    player5 = session.get("player_five")
+    player6 = session.get("player_six")
+    player7 = session.get("player_seven")
+    player8 = session.get("player_eight")
+    player9 = session.get("player_nine")
+    player10 = session.get("player_ten")
+    pvplist = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10]
+    for player in pvplist:
+        player_list.append(careerbest_index.loc[player, ["BPM", "2P", "3P"]].to_json(orient='split'))
+
+    return jsonify(team_list)
 
 @app.route("/about.html")
 def aboutus():
