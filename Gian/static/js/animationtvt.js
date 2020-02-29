ba04dd843645d958d4c93112151ee4d4565e0482
gianmillare <gianmillaregsm@gmail.com>
2020-02-29 12:00:12 -0800

    adding in images for readme previews

--- Gian/static/js/animationtvt.js
+++ Gian/static/js/animationtvt.js
@@ -7,37 +7,39 @@
 // gsap.set(".threePoint", {autoAlpha:1, visibility: "visible", xPercent:-50, yPercent:-50, left:"50%", top:"50%"})
 
 
-function animation() {
-    var bpmOne = 0;
-    var bpmTwo = 0;
-    var threeOne = 0;
-    var twoOne = 0;
-    var threeTwo = 0;
-    var twoTwo = 0;
-    d3.json(`/tvtstats`).then(function(data) {
-        // console.log(data);
 
-    
-        for (var i=0; i < data.length; i +=2){
-            // console.log(value);
-            
-            bpmOne = bpmOne + (data[i]['info']['BPM']);
-            threeOne = threeOne + (data[i]['info']['3P']);
-            twoOne = twoOne + (data[i]['info']['2P']);
-            // console.log(value['info']['BPM']);
-        };
-    
-        for (var j=1; j < data.length; j +=2){
-            // console.log(value);
-            
-            bpmTwo = bpmTwo + (data[j]['info']['BPM']);
-            threeTwo = threeTwo + (data[j]['info']['3P']);
-            twoTwo = twoTwo + (data[j]['info']['2P']);
-            // console.log(value['info']['BPM']);
-        };
-    });
+
+function animation() {
+  var bpmOne = 0;
+  var bpmTwo = 0;
+  var threeOne = 0;
+  var twoOne = 0;
+  var threeTwo = 0;
+  var twoTwo = 0;
+
+  d3.json(`/tvtstats`).then(function(data) {
+    // console.log(data);
+
 
   
+    for (var i=0; i < data.length; i +=2){
+        // console.log(value);
+        
+        bpmOne = bpmOne + (data[i]['info']['BPM']);
+        threeOne = threeOne + (data[i]['info']['3P']);
+        twoOne = twoOne + (data[i]['info']['2P']);
+        // console.log(value['info']['BPM']);
+    };
+
+    for (var j=1; j < data.length; j +=2){
+        // console.log(value);
+        
+        bpmTwo = bpmTwo + (data[j]['info']['BPM']);
+        threeTwo = threeTwo + (data[j]['info']['3P']);
+        twoTwo = twoTwo + (data[j]['info']['2P']);
+        // console.log(value['info']['BPM']);
+    };
+
     if (bpmOne > bpmTwo){
       if((threeOne)*3>(twoOne)*2){
         var timeLine = gsap.timeline();
@@ -64,7 +66,7 @@ function animation() {
           .to("#winnerL", {autoAlpha:0, visibility: "hidden", position: "static", x: 0})
           .to("#loserR", {autoAlpha:0, visibility: "hidden", position: "static", x: 0});
       }
-    } else {
+    } else if (bpmTwo > bpmOne)  {
       if ((threeTwo)*3>(twoTwo)*2){
         var timeLine = gsap.timeline();
         timeLine.to("#threePoint", {autoAlpha:1, visibility: "visible", position: "static", duration: 2.1, x: -25})
@@ -77,26 +79,25 @@ function animation() {
           .to("#loserL", {autoAlpha:1, visibility: "visible", position: "static", duration: 5, x: 0})
           .to("#winnerR", {autoAlpha:0, visibility: "hidden", position: "static", x: 0})
           .to("#loserL", {autoAlpha:0, visibility: "hidden", position: "static", x: 0});
-        } else {
-          var timeLine = gsap.timeline();
-          timeLine.to("#launch", {autoAlpha:1, visibility: "visible", position: "static", duration: 2.8, x: -25})
-            .to("#launch", {autoAlpha:0, visbility: "hidden", position: "absolute"})
-            .to("#air", {autoAlpha:1, visibility: "visible", position: "static", duration: 1.8, x: -25})
-            .to("#air", {autoAlpha:0, visbility: "hidden", position: "absolute"})
-            .to("#dunk", {autoAlpha:1, visibility: "visible", position: "static", duration: 3.8, x: -25})
-            .to("#dunk", {autoAlpha:0, visbility: "hidden", position: "absolute"})
-            .to("#winnerR", {autoAlpha:1, visibility: "visible", position: "static", x: 0})
-            .to("#loserL", {autoAlpha:1, visibility: "visible", position: "static", duration: 5, x: 0})
-            .to("#winnerR", {autoAlpha:0, visibility: "hidden", position: "static", x: 0})
-            .to("#loserL", {autoAlpha:0, visibility: "hidden", position: "static", x: 0});
-        }
+      } else {
+        var timeLine = gsap.timeline();
+        timeLine.to("#launch", {autoAlpha:1, visibility: "visible", position: "static", duration: 2.8, x: -25})
+          .to("#launch", {autoAlpha:0, visbility: "hidden", position: "absolute"})
+          .to("#air", {autoAlpha:1, visibility: "visible", position: "static", duration: 1.8, x: -25})
+          .to("#air", {autoAlpha:0, visbility: "hidden", position: "absolute"})
+          .to("#dunk", {autoAlpha:1, visibility: "visible", position: "static", duration: 3.8, x: -25})
+          .to("#dunk", {autoAlpha:0, visbility: "hidden", position: "absolute"})
+          .to("#winnerR", {autoAlpha:1, visibility: "visible", position: "static", x: 0})
+          .to("#loserL", {autoAlpha:1, visibility: "visible", position: "static", duration: 5, x: 0})
+          .to("#winnerR", {autoAlpha:0, visibility: "hidden", position: "static", x: 0})
+          .to("#loserL", {autoAlpha:0, visibility: "hidden", position: "static", x: 0});
       }
-        bpmOne = 0;
-        bpmTwo = 0;
-  }
-  animation();
-  
-  
-  document.getElementById('tvtWatch').addEventListener('click', function() {
-    animation();
+    }
   });
+}
+animation();
+  
+  
+document.getElementById('tvtWatch').addEventListener('click', function() {
+  animation();
+});
